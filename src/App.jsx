b4b0d95f326f0a521093bd303cbc6d47f3bf4136
Danila <emelchenko1997@yandex.ru>
2024-09-29 18:03:53 +0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.scss";
 import Header from './components/Header/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <Header />
+    </BrowserRouter>
   );
 }
 
