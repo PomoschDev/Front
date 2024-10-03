f3359306c0c6bde8e3bdcb1d5ea5c16d04d86ca1
Danila <emelchenko1997@yandex.ru>
2024-10-04 01:04:19 +0300

    yandexmap

--- src/components/Tape/Tape.jsx
+++ src/components/Tape/Tape.jsx
@@ -1,12 +1,21 @@
 import Card from '../Card/Card';
-import s from './Tape.module.scss'
+import s from './Tape.module.scss';
+import YandexMap from '../UI/YandexMap/YandexMap';
 
 const Tape = () => {
   return (
     <div className={s.container}>
       <div className={s.content}>
         <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
+      <YandexMap />
     </div>
   );
 };
